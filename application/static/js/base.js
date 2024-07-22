9be42071004a51b06ab4f2dff8cc937bb1d45d02
mahi028 <tewarimohit028@gmail.com>
2024-07-22 20:20:44 +0530

    css theme change

--- application/static/js/base.js
+++ application/static/js/base.js
@@ -1,7 +1,7 @@
 function flash(msg){
-    const flash_cont = document.getElementById('flash_cont')
+    const flash_cont = document.getElementById('flash_cont');
     flash_cont.innerHTML = `<div class="alert alert-secondary alert-dismissible fade show" role="alert">
                                 <span>${msg}</span>
                                 <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
-                            </div>`
+                            </div>`;
 }
