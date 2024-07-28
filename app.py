999e7fac4d2427b90a1a4685cb650362d736a8ab
mahi028 <tewarimohit028@gmail.com>
2024-07-28 14:31:09 +0530

    removed errorhandler logic

--- app.py
+++ app.py
@@ -10,18 +10,5 @@ def base():
     form = SeachForm()
     return dict(search_form = form)
 
-#error pages
-@app.errorhandler(404)
-def notFound(e):
-    return render_template('uni/error_template.html', error = 404, error_msg = 'Page Not Found')
-@app.errorhandler(401)
-def notFound(e):
-    return render_template('uni/error_template.html', error = 401, error_msg = 'You Are Not Authorised to access this page.')
-@app.errorhandler(500)
-def notFound(e):
-    return render_template('uni/error_template.html', error = 404, error_msg = 'Internal Server Error')
-
 if __name__ == "__main__":
     app.run(debug = True)
-
-# To delete if not used: python-dateutil, pyparsing, pillow, packaging, numpy, kiwisolver, fonttools, cycler, contourpy, matplotlib
